5c7646662bd4b497417900ba9da299ab58bbfeca
stephanie <moranstephanie83@gmail.com>
2017-02-15 15:25:58 +0000

    built house

---
+++ script.js
@@ -0,0 +1,15 @@
+ TinyTurtle.apply(window); 
+
+ forward(50);
+ left(90);
+ forward(55);
+ left(90);
+ forward(50);
+ left(90);
+ forward(55);
+left(90);
+forward(50);
+left(45);
+forward(40);
+left(90);
+forward(40);
